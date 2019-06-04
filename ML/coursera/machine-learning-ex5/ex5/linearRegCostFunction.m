d0d1caa8c791baef84fdd8e280011ef0fa5f3bf2
Migue Vargas <mvargas@celltrak.com>
2019-06-04 16:55:33 -0300

    Added exercice 5: Bias + Variance analysis

---
+++ ML/coursera/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -0,0 +1,52 @@
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+%regression with multiple variables
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+%   cost of using theta as the parameter for linear regression to fit the 
+%   data points in X and y. Returns the cost in J and the gradient in grad
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost and gradient of regularized linear 
+%               regression for a particular choice of theta.
+%
+%               You should set J to the cost and grad to the gradient.
+%
+
+ 
+function h = hyp(theta, X)
+   h = theta(1) + theta(2) * X(i, 2);
+end
+ 
+% Iterative form 
+sum0 = 0;
+for i = 1 : m
+    sum0 = sum0 + (hyp(theta, X) -  y(i))^2;
+end
+
+n = length(theta); % number of attributes
+sum1 = 0;
+for i = 2 : n
+    sum1 = sum1 + theta(i)^2;
+end
+reg_term = lambda/(2*m) * sum1;
+J = 1/(2*m) * sum0 + reg_term;
+
+
+%Vectorized form
+reg_term = lambda/(2*m) * sumsq(theta(2:end));
+J = 1/(2*m) * (X* theta - y)' * (X* theta - y) + reg_term;
+ 
+
+
+% =========================================================================
+
+grad = grad(:);
+
+end
